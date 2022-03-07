d6afdf0e9a1125c292ab40a706a4eb8edb8cd850
prahlad147 <77727870+prahlad147@users.noreply.github.com>
2022-03-07 21:58:12 +0530

    Add files via upload

---
+++ Game 4cross4/index.js
@@ -0,0 +1,78 @@
+let clickedCard = null;
+// let preventClick = false;
+let combosfound = 0;
+let player1 = 0;
+let player2 = 0;
+
+const colors = [
+    'pink',
+    'yellow',
+    'red',
+    'cyan',
+    'blue',
+    'teal',
+    'orange',
+    'green'
+]
+
+const cards = [...document.querySelectorAll('.card')];
+
+for (let color of colors) {
+    const cardAIndex = parseInt(Math.random() * cards.length);
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`
+    cardA.setAttribute(`data-color`, color);
+
+    const cardBIndex = parseInt(Math.random() * cards.length);
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`
+    cardB.setAttribute(`data-color`, color);
+}
+
+function onCardClicked(e) {
+    const target = e.currentTarget;
+
+    if (target === clickedCard || target.className.includes('done')) {
+        return;
+    }
+
+    // console.log(target.className);
+    target.className = target.className.replace('color-hidden', '').trim();
+    target.className += ' done';
+
+    // console.log(target.getAttribute('data-color'));
+
+    if (!clickedCard) {
+        clickedCard = target;
+    } else if (clickedCard) {
+        if (clickedCard.getAttribute('data-color') === target.getAttribute('data-color')) {
+            // clickedCard.className += ' done';
+            // target.className = +' done';
+            // console.log("Cards are equal");
+            clickedCard = null;
+            combosfound++;
+            // player1++;
+            // console.log(player1);
+            // preventClick = true;
+        } else {
+
+            // console.log("Cards are not equal");
+            setTimeout(() => {
+                clickedCard.className = clickedCard.className.replace('done', '').trim() + 'color-hidden';
+                target.className = target.className.replace('done', '').trim() + 'color-hidden';
+
+                clickedCard = null;
+                // preventClick = false;
+            }, 300);
+        }
+
+
+    }
+    if (combosfound === 8) {
+        alert("You Win");
+    }
+
+
+}
